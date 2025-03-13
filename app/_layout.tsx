e9bf84bc70619a0200b0e1c0a4803bcf9483e1a6
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2025-03-12 21:39:34 -0300

    Code Clean up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,6 @@ export default function RootLayout() {
             <Stack>
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
                 <Stack.Screen name="+not-found"/>
-                <Tabs.Screen name="login" options={{title: 'Login'}}/>
             </Stack>
             <StatusBar style="light" />
 
